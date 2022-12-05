15edde69f07f52f63ec82472550d4938b05074b6
lhh1171 <2807652103@qq.com>
2022-12-05 15:51:27 +0800

    micro

--- src/TestVgo/go.mod
+++ src/TestVgo/go.mod
@@ -1,3 +1,17 @@
 module TestVgo
 
-go 1.19
+go 1.13
+
+require (
+	github.com/evanphx/json-patch/v5 v5.5.0 // indirect
+	github.com/golang/protobuf v1.5.2
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/micro/micro/v3 v3.15.0
+	github.com/miekg/dns v1.1.50 // indirect
+	github.com/urfave/cli v1.22.4 // indirect
+	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/net v0.2.0 // indirect
+	google.golang.org/protobuf v1.28.1
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- src/TestVgo/main.go
+++ src/TestVgo/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	proto "TestVgo/proto"
+	"github.com/micro/micro/v3/service"
+)
+
 func main() {
-	
+	// create and initialise a new service
+	srv := service.New()
+
+	// create the proto client for helloworld
+	client := proto.NewHelloworldService("helloworld", srv.Client())
+
+	// call an endpoint on the service
+	rsp, err := client.Call(context.Background(), &proto.Request{
+		Name: "John",
+	})
+	if err != nil {
+		fmt.Println("Error calling helloworld: ", err)
+		return
+	}
+
+	// print the response
+	fmt.Println("Response: ", rsp.Msg)
+
+	// let's delay the process for exiting for reasons you'll see below
+	time.Sleep(time.Second * 5)
 }
