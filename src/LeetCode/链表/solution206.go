e8511f2cbe0c4b5d38070f15f9258cabe4a37be8
lhh1171 <2807652103@qq.com>
2023-03-23 20:02:36 +0800

    pp

---
+++ src/LeetCode/链表/solution206.go
@@ -0,0 +1,28 @@
+package 链表
+
+// 双指针
+func reverseList1(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
+
+// 递归
+func reverseList2(head *ListNode) *ListNode {
+	return help(nil, head)
+}
+
+func help(pre, head *ListNode) *ListNode {
+	if head == nil {
+		return pre
+	}
+	next := head.Next
+	head.Next = pre
+	return help(head, next)
+}
