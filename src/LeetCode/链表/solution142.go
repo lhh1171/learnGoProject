37fe1c6ccf25d4caf2e202ca0c87a6148a0726e6
lhh1171 <2807652103@qq.com>
2023-03-28 23:13:07 +0800

    ll

---
+++ src/LeetCode/链表/solution142.go
@@ -0,0 +1,17 @@
+package 链表
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			for slow != head {
+				slow = slow.Next
+				head = head.Next
+			}
+			return head
+		}
+	}
+	return nil
+}
